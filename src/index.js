07d7c54da3dfebd2866708ae5b959b5307b6d465
TracyAntonia <aantonjuguna@gmail.com>
2023-07-04 16:14:22 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  const renderTodoItem = function (e) {
+  let form = document.querySelector("create-task-form");
+  form.addEventListener('submit', (e) => {
     e.preventDefault();
-}}
-);
+    newToDo(e.target.new-task-description.value);
+    // form.reset{};
+  });
+
+
+  function newToDo(todo){
+    const para = document.createElement('p');
+    const btn = document.createElement('button');
+    btn.textContent = " X ";
+    para.textContent = ` ${todo} `;
+    para.appendChild(btn);
+  }
+
+});
+
+
+function deleteTask(e){
+  e.target.parentNode.remove();
+}
+
